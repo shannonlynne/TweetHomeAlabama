c9ed4a4f889e008df24ff5270018d17d971c6dc4
Shannon M <shannon.morrell2@gmail.com>
2023-11-26 22:14:29 -0600

    Update property of Bird

--- TweetHomeAlabama.Application/Models/Bird.cs
+++ TweetHomeAlabama.Application/Models/Bird.cs
@@ -5,6 +5,6 @@
         public int Name { get; set; }
         public string? Info { get; set; }
         public string? ImageUrl { get; set; }
-        public List<BirdTrait> BirdTraits { get; set; } = new List<BirdTrait>();
+        public List<string> BirdTraits { get; set; } = new List<string>();
     }
 }
