ee17cf2f20c4709bc8ad74ed5cf9af56fc414257
Shannon M <shannon.morrell2@gmail.com>
2023-12-03 20:19:00 -0600

    Updates to the context and connection string

--- TweetHomeAlabama.Infrastructure/DataContext/TweetHomeAlabamaDbContext.cs
+++ TweetHomeAlabama.Infrastructure/DataContext/TweetHomeAlabamaDbContext.cs
@@ -5,37 +5,35 @@ namespace TweetHomeAlabama.Infrastructure.DataContext
 {
     public class TweetHomeAlabamaDbContext : DbContext
     {
-        public TweetHomeAlabamaDbContext(DbContextOptions<TweetHomeAlabamaDbContext> options)
-            : base(options) { }
+        public DbSet<Bird> Birds => Set<Bird>();
 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    var connectionString = _configuration.GetConnectionString("ShannonApps");
-
-        //    if (connectionString is not null)
-        //        optionsBuilder.UseNpgsql(connectionString);
-
-        //}
-        //    // => optionsBuilder.UseNpgsql("Server = localHost:5433; Database=ShannonApps;Trusted_Connection=True;");
-
-        protected override void OnModelCreating(ModelBuilder builder)
+        public TweetHomeAlabamaDbContext(DbContextOptions options) : base(options)
         {
-            builder.Entity<Bird>(entity =>
-            {
-                entity.ToTable("Bird");
 
-                entity.Property<int>("BirdId")
-                 .ValueGeneratedOnAdd();
-                entity.HasKey("BirdId");
-
-                entity.Property(x => x.Name);
-                entity.Property(x => x.Info);
-                entity.Property(x => x.Traits);
-                entity.Property(x => x.ImageId);
-            });
         }
 
-        public DbSet<Bird> Birds => Set<Bird>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            {
+                modelBuilder.Entity<Bird>(entity =>
+                {
+                    entity.ToTable("Bird", "Bird");
+
+                    entity.HasKey(bird => new
+                    {
+                        bird.BirdId
+                    });
+
+
+                    entity.Property(x => x.BirdId).HasColumnName("BirdId");
+                    entity.Property(x => x.Info).HasColumnName("Info");
+                    entity.Property(x => x.ImageId).HasColumnName("ImageId");
+                    entity.Property(x => x.Traits).HasColumnName("Traits");
+                });
+
+                base.OnModelCreating(modelBuilder);
+            }
+        }
     }
 }
 
